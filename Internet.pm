956e12ce6a0244aaebcf48e6794381e467b0150f
Jan Dubois <jand@activestate.com>
2008-06-14 01:32:05 +0000

    Import of JDB/Win32-Internet-0.084 from CPAN.
    gitpan-cpan-distribution: Win32-Internet
    gitpan-cpan-version:      0.084
    gitpan-cpan-path:         JDB/Win32-Internet-0.084.tar.gz
    gitpan-cpan-author:       JDB
    gitpan-cpan-maturity:     released

--- Internet.pm
+++ Internet.pm
@@ -8,6 +8,8 @@
 # Version: 0.08  (14 Feb 1997)
 # Version: 0.081 (25 Sep 1999)
 # Version: 0.082 (04 Sep 2001)
+# Version: 0.083 (15 Apr 2008)
+# Version: 0.084 (13 Jun 2008)
 #
 #######################################################################
 
@@ -151,7 +153,7 @@ sub AUTOLOAD {
 #######################################################################
 # STATIC OBJECT PROPERTIES
 #
-$VERSION = "0.082";
+$VERSION = "0.084";
 
 %callback_code = ();
 %callback_info = ();
@@ -2797,7 +2799,18 @@ use, refer to the Microsoft Win32 Internet Functions document.
 
 =over
 
+=item * 0.084 (13 Jun 2008)
+
+Move sample.pl and test-async.pl into the eg/ subdirectory to avoid
+installing them into the Perl/bin directory.
+
+Use T_BOOL instead of T_IV for BOOL typemap entry so that the code
+doesn't warn on undef.
+
 =item * 0.083 (15 Apr 2008)
+
+This release unfortunately did not update $Win32::Internet::VERSION.
+Only META.yml reffered to this release as version 0.083.
 
 =over
 
@@ -2805,9 +2818,15 @@ use, refer to the Microsoft Win32 Internet Functions document.
 
 New version for separate upload to CPAN.
 
+=item *
+
 Updated email addresses (but some stale references to divinf.it still remain).
 
+=item *
+
 Simplified Makefile.PL; added META.yml; added ppport.h.
+
+=item *
 
 Renamed test.pl to sample.pl because it doesn't generate TAP output.
 
